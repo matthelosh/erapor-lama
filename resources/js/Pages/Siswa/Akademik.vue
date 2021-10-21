5e8bb1fdf4dfadc2e8e2cf4b3fc04b02e7bf2f61
matthelosh <matthelosh@gmail.com>
2021-10-21 12:09:35 +0700

    Change Siswa Account Default Password

---
+++ resources/js/Pages/Siswa/Akademik.vue
@@ -0,0 +1,31 @@
+<template>
+    <Layout :title="page_title">
+        <div class="container">
+            
+        </div>
+    </Layout>
+</template>
+
+<script>
+import Layout from '../../Layout/Dashboard'
+export default {
+    components: {
+        Layout
+    },
+    props: {
+        user: Object,
+        page_title: String,
+        menus: Array,
+        datas: Array
+    },
+    created() {
+        // console.log(this.$page.props.menus)
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background: #fff;
+    }
+</style>
