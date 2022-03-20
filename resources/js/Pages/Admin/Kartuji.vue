7346901b1dde92bdba0ab2c69bb91b5dff51ad62
matthelosh <matthelosh@gmail.com>
2022-03-20 12:11:32 +0700

    replace drop box with button to select rombel

--- resources/js/Pages/Admin/Kartuji.vue
+++ resources/js/Pages/Admin/Kartuji.vue
@@ -9,92 +9,11 @@
                                 <v-icon>mdi-card</v-icon>
                                 Kartu Ujian
                                 <v-spacer class="my-1"></v-spacer>
-                                <v-select dense hide-details label="Rombel" outlined rounded
-                                    :items="rombels"
-                                    item-text="label"
-                                    item-value="kode_rombel"
-                                    @change="selectRombel"
-                                ></v-select>
                                 <v-btn rounded class="mx-3" color="primary" @click="cetak"><v-icon>mdi-printer</v-icon></v-btn>
                             </v-toolbar>
                         </v-card-title>
                         <v-card-text>
-                            <!-- <v-sheet v-if="rombel" class="my-5" ref="sheet" id="kartu">
-                                <div class="mainRow"
-                                    style="display:flex;flex-wrap:wrap;"
-                                >
-                                    <span class="kartu"
-                                        style="width:47%; flex-grow:50%;margin: 5px 10px;box-sizing:border-box;" 
-                                        v-for="(siswa,i) in rombel[0].siswas"
-                                        :key="i"
-                                        d-print-flex
-                                    >
-                                        <v-card style="border: 1px solid black;padding:10px;">
-                                            <v-card-title class="text-center">
-                                                <v-row style="position:relative;display:flex; justify-content:start;">
-                                                    <img src="/img/malangkab.png" width="50" style="position:absolute;" class="logo" />
-                                                    <div class="text-center" style="margin-left: 20px;flex-grow:1;">
-                                                        <h4 class="my-0 pa-0" style="line-height:1em;">
-                                                            KARTU PESERTA UJIAN <br>
-                                                            SD NEGERI 1 BEDALISODO <br>
-                                                            TAHUN PELAJARAN {{ $page.props.periode_aktif.tapel}}</h4>
-                                                    </div>
-                                                </v-row>
-                                            </v-card-title>
-                                            <v-card-text>
-                                                <v-divider></v-divider>
-                                                <h4 class="text-center my-2">DATA PESERTA</h4>
-                                                <table width="100%">
-                                                    <tr>
-                                                        <td style="width: 30%">No. Peserta</td>
-                                                        <td class="mx-2">:</td>
-                                                        <td class="pl-2">{{ siswa.nisn }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Nama</td>
-                                                        <td class="mx-2">:</td>
-                                                        <td class="pl-2" style="vertical-align:top;">{{ siswa.nama }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Tempat, Tgl Lahir</td>
-                                                        <td class="mx-2">:</td>
-                                                        <td class="pl-2">{{ siswa.tempat_lahir }}, {{siswa.tanggal_lahir}}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Kelas</td>
-                                                        <td class="mx-2">:</td>
-                                                        <td class="pl-2">{{ rombel[0].label }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Sekolah Asal</td>
-                                                        <td class="mx-2">:</td>
-                                                        <td class="pl-2">{{ $page.props.sekolah.nama_sekolah }}</td>
-                                                    </tr>
-                                                </table>
-                                                <br>
-                                                <br>
-                                                <br>
-                                                <v-row>
-                                                    <v-col style="margin-left:50%; font-size:1em;">
-                                                        Kepala Sekolah
-                                                        <br>
-                                                        <br>
-                                                        <br>
-                                                        <br>
-                                                        <b><u>{{$page.props.sekolah.ks.name}}</u></b><br>
-                                                        NIP. {{$page.props.sekolah.ks.nip}}
-                                                    </v-col>
-                                                </v-row>
-                                            </v-card-text>
-                                        </v-card>
-                                        <div v-if="(i+1)%6 == 0" style="page-break-after:always;" class="pageBreak">
-                                            ===================================================================
-                                        </div>
-                                    </span>
-                                    
-                                </div>
-                            </v-sheet> -->
-                            <span v-if="pages.length > 0" id="print">
+                            <span v-if="rombel" id="print">
                                <v-sheet v-for="(page,i) in pages" :key="i" class="my-2 page" color="#efefef" :style="(i+1) == pages.length ? 'page-break-after:avoid;' : 'page-break-after:always;'">
                                    <div v-for="(siswa,s) in page" :key="s" style="width:46%;display:inline-block;" class="kartu">
                                        <div class="header" style="position:relative;border-bottom: 5px double black;padding: 10px;">
@@ -148,6 +67,20 @@
                                        </div>
                                    </div>
                                </v-sheet>
+                            </span>
+                            <span v-else>
+                                <v-container>
+                                    <v-row>
+                                        <v-col>
+                                            <h2>Pilih Rombel</h2>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col cols="2" v-for="rombel in rombels" :key="rombel.id">
+                                            <v-btn block color="primary" @click="selectRombel(rombel.kode_rombel)">{{rombel.label}}</v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-container>
                             </span>
                         </v-card-text>
                     </v-card>
