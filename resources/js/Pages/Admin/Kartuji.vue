433433e6430a3d08a600772d388882f33cc0f472
matthelosh <matthelosh@gmail.com>
2022-03-20 12:40:55 +0700

    Fix Insya Allah Kartu Ujian

--- resources/js/Pages/Admin/Kartuji.vue
+++ resources/js/Pages/Admin/Kartuji.vue
@@ -9,19 +9,25 @@
                                 <v-icon>mdi-card</v-icon>
                                 Kartu Ujian
                                 <v-spacer class="my-1"></v-spacer>
-                                <v-btn rounded class="mx-3" color="primary" @click="cetak"><v-icon>mdi-printer</v-icon></v-btn>
+                                <span  v-if="rombel != null"> 
+                                    <v-btn rounded class="mx-3" color="info" @click="rombel = null"><v-icon>mdi-format-list-text</v-icon> Pilih Rombel</v-btn>
+                                    <v-btn rounded class="mx-3" color="info" @click="cetak" ><v-icon>mdi-printer</v-icon> Cetak</v-btn>
+                                </span>
+                                <span v-else>
+                                    <v-select dense hide-details :items="['PENILAIAN HARIAN', 'PENILAIAN TENGAH SEMESTER', 'PENILAIAN AKHIR SEMESTER', 'UJIAN SEKOLAH', 'UJIAN PRAKTEK', 'TRY OUT']" v-model="ujian" label="Jenis Penilaian" solo></v-select>
+                                </span>
                             </v-toolbar>
                         </v-card-title>
                         <v-card-text>
-                            <span v-if="rombel" id="print">
+                            <span v-if="rombel != null" id="print">
                                <v-sheet v-for="(page,i) in pages" :key="i" class="my-2 page" color="#efefef" :style="(i+1) == pages.length ? 'page-break-after:avoid;' : 'page-break-after:always;'">
                                    <div v-for="(siswa,s) in page" :key="s" style="width:46%;display:inline-block;" class="kartu">
                                        <div class="header" style="position:relative;border-bottom: 5px double black;padding: 10px;">
                                            <img src="/img/malangkab.png" alt="Logo" width="50" style="position:absolute;" class="logo">
                                            <h4 class="my-0 pa-0" style="line-height:1.3em;text-align:center;">
-                                                    KARTU PESERTA UJIAN <br>
+                                                    KARTU PESERTA {{ujian}} <br>
                                                     SD NEGERI 1 BEDALISODO <br>
-                                                    TAHUN PELAJARAN {{ $page.props.periode_aktif.tapel}}</h4>
+                                                    TAHUN PELAJARAN {{ $page.props.periode_aktif.tapel}} SEM {{$page.props.periode_aktif.semester == '1' ? 'GANJIL' : 'GENAP'}}</h4>
                                        </div>
                                        <div class="data-siswa" style="padding: 0 10px;">
                                             <h4 class="text-center" style="margin: 10px auto;">DATA PESERTA</h4>
@@ -75,7 +81,15 @@
                                             <h2>Pilih Rombel</h2>
                                         </v-col>
                                     </v-row>
-                                    <v-row>
+                                    <v-row  v-if="loading">
+                                        <v-col cols="12" class="d-flex justify-center">
+                                            <span class="text-center">
+                                                <v-progress-circular color="purple" indeterminate :size="50" :width="7"></v-progress-circular>
+                                                <h3 class="text-center">Data Rombel sedang Diambil...</h3>
+                                            </span>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row v-else>
                                         <v-col cols="2" v-for="rombel in rombels" :key="rombel.id">
                                             <v-btn block color="primary" @click="selectRombel(rombel.kode_rombel)">{{rombel.label}}</v-btn>
                                         </v-col>
@@ -105,7 +119,9 @@ export default {
     },
     data: () => ({
         rombels: [],
-        rombel: null
+        rombel: null,
+        loading: false,
+        ujian: '',
     }),
     methods: {
         cetak(){
@@ -151,11 +167,13 @@ export default {
             win.print()
         },
         getRombels() {
+            this.loading = true
             axios({
                 method: 'post',
                 url: '/admin/rombel?periode='+this.$page.props.periode_aktif.kode_periode
             }).then(res => {
                 this.rombels = res.data.rombels
+                this.loading = false
             })
         },
         selectRombel(e){
